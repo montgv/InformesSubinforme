b0ef3a484d3a3344da5e7a49cb55bcf03951bbba
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2023-02-28 22:45:19 +0100

    Agregar archivos de proyecto.

---
+++ InformesSubinforme/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace InformesSubinforme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dataSetMaestroDetalle = new InformesSubinforme.DataSetMaestroDetalle();
+            this.cochesVendidosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cochesVendidosTableAdapter = new InformesSubinforme.DataSetMaestroDetalleTableAdapters.CochesVendidosTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetMaestroDetalle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cochesVendidosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.cochesVendidosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "InformesSubinforme.ReportEmpleado.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(32, 38);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1043, 389);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dataSetMaestroDetalle
+            // 
+            this.dataSetMaestroDetalle.DataSetName = "DataSetMaestroDetalle";
+            this.dataSetMaestroDetalle.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cochesVendidosBindingSource
+            // 
+            this.cochesVendidosBindingSource.DataMember = "CochesVendidos";
+            this.cochesVendidosBindingSource.DataSource = this.dataSetMaestroDetalle;
+            // 
+            // cochesVendidosTableAdapter
+            // 
+            this.cochesVendidosTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetMaestroDetalle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cochesVendidosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private DataSetMaestroDetalle dataSetMaestroDetalle;
+        private System.Windows.Forms.BindingSource cochesVendidosBindingSource;
+        private DataSetMaestroDetalleTableAdapters.CochesVendidosTableAdapter cochesVendidosTableAdapter;
+    }
+}
+
---
+++ InformesSubinforme/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InformesSubinforme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dataSetMaestroDetalle.CochesVendidos' Puede moverla o quitarla según sea necesario.
+            this.cochesVendidosTableAdapter.Fill(this.dataSetMaestroDetalle.CochesVendidos);
+
+            this.reportViewer1.RefreshReport();
+
+            this.reportViewer1.LocalReport.SubreportProcessing += LocalReport_SubreportProcessing;
+        }
+
+        private void LocalReport_SubreportProcessing(object sender, SubreportProcessingEventArgs e)
+        {
+            //Guardamos el id del comercial
+            var idComercial = int.Parse(e.Parameters["idComercial"].Values.First());
+            
+            //Creamos un dataset y lo rellenamos con la consulta
+            DataSetMaestroDetalle.CochesVendidosDataTable coches = new DataSetMaestroDetalle.CochesVendidosDataTable();
+            this.cochesVendidosTableAdapter.FillBy(coches, idComercial.ToString());
+            //Enlazamos con el DataSet del conjunto de datos del subinforme
+            e.DataSources.Add(new ReportDataSource("DataSet1", (DataTable)coches));
+        }
+    }
+}
